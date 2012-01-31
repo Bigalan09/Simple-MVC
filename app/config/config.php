659ef0ce614f5e635e0491001182a225825a6886
Alan Gardner <bigalan09@gmail.com>
2012-01-31 20:25:37 +0000

    Added in database configuration for use with Models.

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$db_host			=	"localhost";
+$db_user			=	"database_username";
+$db_password		=	"changeMe";
+$db_table			=	"database_table";
+
+$default_time_zone	=	"Europe/London";
+
+$encrypt_key		=	"changeMe";
+
+?>
