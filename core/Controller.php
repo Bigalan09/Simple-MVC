ac70714f857c28e1d6fc56baa517af633d34602e
Alan Gardner <bigalan09@gmail.com>
2012-02-03 23:14:15 +0000

    Added render methid which can be used to render a new view.

--- core/Controller.php
+++ core/Controller.php
@@ -3,6 +3,14 @@
 // contain's methods for all the user defined controllers
 class Controller extends Object {
 	
+	public $autoRender = true;
+	public $name = null;
+	
+	public function __construct() {
+		if ($this->name === null) {
+			$this->name = get_class($this);
+		}
+	}
 	
 	public function load_model($name) {
 		$model_path = APP_PATH . '/models/' . $name . 'Model.php';
@@ -18,6 +26,35 @@ class Controller extends Object {
 			die('The file <strong>' . $name . 'Model.php</strong> could not be found at <pre>' . APP_PATH . '/models/' . $name . 'Model.php</pre>');
 		
 		return $tmp_class;
+	}
+	
+	public function render($view = null, $layout = null){
+		$this->beforeRender();
+		$this->autoRender = false;
+		
+		$this->name = get_class($this);
+		
+		if ($view === null) {				
+			$view = Router::view_path($this->name);
+		} else {
+			if (strpos($view, '/')) {
+				$r = explode('/', $view);
+				$view = Router::view_path($r[0], $r[1]);
+			} else {
+				$view = Router::view_path($this->name, $view);
+			}
+		}
+		
+		if ($layout === null) {
+			$layout = Router::get_layout($this->name);
+		} else {
+			$layout = Router::get_layout($layout);
+		}
+		
+		Router::render($view, $layout);
+		$this->afterRender();
+		
+		return true;
 	}
 	
 	public function beforeFilter() {
